5ea6d30bc2ef20cfb5d8792be923093b85a20f9a
damnDN <dnegi.py@gmail.com>
2026-07-27 22:20:19 +0530

    Created a uniform req-res pipeline

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,7 @@ import User from "../models/User.js";
 import RefreshToken from "../models/RefreshToken.js";
 
 import asyncHandler from "../middlewares/asyncHandler.js";
-
+import crypto from "crypto";
 import hash from "../utils/generateHash.js";
 import createJWT from "../utils/createJWT.js";
 import createRefToken from "../utils/createRefreshToken.js";
@@ -16,12 +16,12 @@ const createUser = asyncHandler(async (req, res, next) => {
 
   if (!username || !email || !password) {
     //see the log in notion: WHY YOU MUST USE CUSTOM ERROR(class)
-    throw new AppError("FILL ALL THE DETAILS", 400);
+    throw new AppError("Fill all details", 400, "MISSING_REQUIRED_FIELDS");
   }
 
   const userExists = await User.findOne({ email });
   if (userExists) {
-    throw new AppError("USER ALREADY EXISTS", 400);
+    throw new AppError("USER ALREADY EXISTS", 400, "USER_ALREADY_EXISTS");
   }
 
   //Hashing the password: handled in model
@@ -31,6 +31,7 @@ const createUser = asyncHandler(async (req, res, next) => {
   await newUser.save();
   const accToken = createJWT(newUser._id);
   const refToken = createRefToken(res);
+
   const newRefToken = new RefreshToken({
     userId: newUser._id,
     token: refToken,
@@ -49,7 +50,7 @@ const createUser = asyncHandler(async (req, res, next) => {
 const loginUser = asyncHandler(async (req, res) => {
   const { field, password } = req.body;
   if (!field || !password) {
-    throw new AppError("Fill all details", 401);
+    throw new AppError("Fill all details", 400, "MISSING_REQUIRED_FIELDS");
   }
 
   //no user can have any non-alphanumeric character in username so following is immaculate
@@ -57,11 +58,20 @@ const loginUser = asyncHandler(async (req, res) => {
 
   const userExists = await User.findOne({ [key]: field });
   if (!userExists) {
-    return res.status(200).send("USER DOES NOT EXIST, please SIGN UP");
+    throw new AppError(
+      "User does not exist, please sign up.",
+      404,
+      "USER_NOT_FOUND",
+      {
+        field: "User does not exist",
+      },
+    );
   }
   const isPasswordValid = await bcrypt.compare(password, userExists.password);
   if (!isPasswordValid) {
-    throw new AppError("Invalid Password", 401);
+    throw new AppError("Invalid Password", 401, "INVALID_CREDENTIALS", {
+      password: "Invalid password.",
+    });
   }
   const accToken = createJWT(userExists._id);
   const refToken = createRefToken(res);
@@ -82,6 +92,29 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const logOutUser = asyncHandler(async (req, res) => {
+  const incomingRefToken = req.cookies?.refreshToken;
+  if (!incomingRefToken) {
+    throw new AppError("Refresh token missing", 401, "REFRESH_TOKEN_MISSING");
+  }
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(incomingRefToken)
+    .digest("hex");
+  const token = await RefreshToken.findOneAndUpdate(
+    {
+      userId: req.user._id,
+      token: hashedToken,
+      is_Used: false,
+    },
+    { $set: { is_Used: true } },
+  );
+  if (!token) {
+    throw new AppError(
+      "Invalid or already used refresh token",
+      401,
+      "INVALID_REFRESH_TOKEN",
+    );
+  }
   res.clearCookie("refreshToken", {
     //aow, i sense need of design pattern
     httpOnly: true,
@@ -89,7 +122,6 @@ const logOutUser = asyncHandler(async (req, res) => {
     sameSite: "strict",
   });
   //FYI: new Date(0) is c/d epoch date.
-  await User.findOneAndDelete({ email: req.user.email });
   res.status(200).json({ message: "Logged out successfully" });
 });
 
@@ -108,7 +140,8 @@ const getUser = asyncHandler(async (req, res) => {
 
 const updateUser = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
-  if (!user) throw new AppError("User not found", 401);
+
+  if (!user) throw new AppError("User not found", 400, "USER_NOT_FOUND");
   user.username = req.body.username || user.username;
   user.email = req.body.email || user.email;
 
